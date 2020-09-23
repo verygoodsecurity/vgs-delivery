5f4955b8a778453bb71076afa8b2d0140bf358f4
ahio <stepan.lopushanskyi@vgs.io>
2020-09-23 13:05:31 +0300

    small code refactoring

--- src/waitImage.ts
+++ src/waitImage.ts
@@ -2,14 +2,12 @@ import qs from 'query-string';
 
 export const waitImage = (img: HTMLImageElement, url: string) =>
   new Promise((resolve, reject) => {
-    const body = document.getElementsByTagName('body')[0];
     const cacheFree = Math.random() * 1000000000;
     const parsedUrl = url.split('?');
     const cacheQueryString = qs.stringify({ cacheFree });
     let urlInstance = url.slice();
     urlInstance +=
       parsedUrl && parsedUrl[1] ? `&${cacheQueryString}` : cacheQueryString;
-    // urlInstance.searchParams.set('cacheFree', `${cacheFree}`);
     img.onerror = () => {
       reject(`${url} request failed`);
     };
@@ -18,5 +16,5 @@ export const waitImage = (img: HTMLImageElement, url: string) =>
     };
 
     img.src = urlInstance;
-    body.appendChild(img);
+    document.body.appendChild(img);
   });
