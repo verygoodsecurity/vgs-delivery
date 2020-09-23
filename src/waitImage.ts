04c6652603a00ab3fe23a130c4d268296597bf24
Stepan Lopushansky <stepan.te.ua@gmail.com>
2020-09-23 14:10:39 +0300

    Added ie11 build (#2)
    * fix lint
    
    * try to get image via image tag recursively
    
    * try to get image via image tag recursively
    
    * small code refactoring

--- src/waitImage.ts
+++ src/waitImage.ts
@@ -1,13 +1,20 @@
-export const waitImage = (img: HTMLImageElement, url: string) => new Promise((resolve, reject) => {
-  const urlInstance = new URL(url);
-  const cacheFree = Math.random() * 1000000000;
-  urlInstance.searchParams.set('cacheFree', `${cacheFree}`);
-  img.onerror = () => {
-    reject(`${url} request failed`);
-  };
-  img.onload = () => {
-    resolve(url);
-  };
+import qs from 'query-string';
 
-  img.src = urlInstance.href;
-});
+export const waitImage = (img: HTMLImageElement, url: string) =>
+  new Promise((resolve, reject) => {
+    const cacheFree = Math.random() * 1000000000;
+    const parsedUrl = url.split('?');
+    const cacheQueryString = qs.stringify({ cacheFree });
+    let urlInstance = url.slice();
+    urlInstance +=
+      parsedUrl && parsedUrl[1] ? `&${cacheQueryString}` : cacheQueryString;
+    img.onerror = () => {
+      reject(`${url} request failed`);
+    };
+    img.onload = () => {
+      resolve(url);
+    };
+
+    img.src = urlInstance;
+    document.body.appendChild(img);
+  });
